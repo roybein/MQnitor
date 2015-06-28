326403c18add5f6f9a8085d5d3e30f8d522aab4a
roybein <roybein@gmail.com>
2015-06-28 22:40:59 +0800

    1.add Plan 2.refine filed of Sensor, Relay 3.add link between Plan and Sensor, Relay

--- server/bsMonitor.js
+++ server/bsMonitor.js
@@ -19,38 +19,21 @@ mqttClient.on("connect", function() {
 });
 */
 
-// call a fake module
-fakelog();
-/*
-// init fake sensors
-sensorAll = new SensorBase(new Mongo.Collection("sensors"));
-var sensorLight = new Sensor(1, "light", "lux", 777, "a fake sensor");
-sensorAll.addSensor(sensorLight);
-*/
-/*
-var sensorTemperature = new Sensor( Sensors, 4, "temperature", "C", 24, "a fake sensor");
-var sensorHumidity = new Sensor( Sensors, 7, "humidity", "%", 65, "a fake sensor");
-var sensorVoltage = new Sensor( Sensors, 9, "voltage", "volt", 3200, "a fake sensor");
-*/
-/*
-// int fake relays
-var relayMasterPower = new Relay( Relays, 1, "master power", "on", null);
-*/
-/*
-// int fake Judge
-var judgeEleTest = new JudgeElement(1, sensorLight.index, 500, 600, true);
-var judgeTest = new Judge(JudgeGroup, 1, []);
-judgeTest.addElement(judgeEleTest);
-console.log(judgeTest.judgeMap[1]);
-*/
-
 // register a listener on data collection Sensors
 var querySensors = sensorAll.collec.find();
 
 var handle = querySensors.observeChanges({
 		changed: function(id, fields) {
-			// Call judge here
-			console.log(fields);
+			console.log("id=", id, "fields=", fields);
+            if(Object.keys(fields) == 'value') {
+                var sensor = sensorAll.getSensor(id);
+                  
+                var pIdG = sensorAll.collec.findOne({_id:id}).planIdGroup;
+                pIdG.forEach(function(elem, index, group) {
+                    var plan = planAll.getPlan(elem);
+                    console.log("check plan:", plan);
+                });
+            }
 		}
 });
 
