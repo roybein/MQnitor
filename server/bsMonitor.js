71c69c91d52aadf7a95a42498975a751042b6eeb
roybein <roybein@gmail.com>
2015-09-12 18:23:53 +0800

    add communicator
    1.add communicator, it will connect mqtt broker.
    2.parse the mqtt message and handle it.
    3.now the basic update input value works

--- server/bsMonitor.js
+++ server/bsMonitor.js
@@ -1,31 +1,3 @@
-// a mqtt test
-/*
-mqttClient = mqtt.connect('mqtt:accrete.org');
-
-mqttClient.on("connect", function() {
-		console.log("connected accrete mqtt");
-
-		mqttClient.publish("presence", "hello mqtt from meteor", function() {
-				console.log("publish done");
-		});
-
-		mqttClient.subscribe("/nodemcu/input", function() {
-				console.log("subscribe done");
-		});
-
-		mqttClient.on("message", function(p1, p2) {
-			console.log(p1, p2.toString());
-            if (parseInt(p2) > 900) {
-                console.log("turn on");
-                mqttClient.publish("/nodemcu/output", "1");
-            } else {
-                console.log("turn off");
-                mqttClient.publish("/nodemcu/output", "0");
-            }
-		});
-
-});
-*/
 
 // register a listener on data collection Sensors
 var queryInput = contactAll.collec.find({direction:"input"});
---
+++ server/communicator.js
@@ -0,0 +1,109 @@
+// a mqtt test
+
+mqttClient = mqtt.connect('mqtt:123.57.208.39');
+
+mqttClient.on("connect", function() {
+    console.log("connected mqtt broker");
+
+    mqttClient.publish("presence", "hello mqtt from bsMonitor", function() {
+        console.log("publish done");
+    });
+
+    mqttClient.subscribe("/#", function() {
+            console.log("subscribe done");
+    });
+
+    mqttClient.on("message", onMsgMqtt);
+/*
+ function(p1, p2) {
+        var Fiber = Npm.require('fibers');
+        Fiber(function() {
+            console.log("I am in a fiber");
+            console.log(contactAll.collec.findOne({direction:"input"}));
+        }).run();
+        //console.log(Fiber(contactAll.collec.findOne("direction:input")));
+    });
+/*
+        if (parseInt(p2) > 900) {
+            console.log("turn on");
+            mqttClient.publish("/nodemcu/output", "1");
+        } else {
+            console.log("turn off");
+            mqttClient.publish("/nodemcu/output", "0");
+        }
+*/
+});
+
+onMsgMqtt = function(topic, message) {
+    var tpArray = topic.split("/"); 
+    console.log(tpArray, message.toString());
+    if (tpArray.shift() !== "") {
+        console.log("unsupported message start without /");
+        return -1;
+    }
+
+    var tpKey = tpArray.shift();
+    switch(tpKey) {
+        case "up":
+            onMsgUp(tpArray, message);
+            break;
+        case "down":
+            console.log("get down message");
+            break;
+        default:
+            console.log("unsupported topic key:", tpKey );
+    }
+}
+
+onMsgUp = function(topic, message) {
+    var tpKey = topic.shift();
+    switch(tpKey) {
+        case "bs":
+            onMsgUpBs(topic, message);
+            break;
+        default:
+            console.log("unsupported topic key:", tpKey );
+    }
+}
+
+onMsgUpBs = function(topic, message) {
+    var tpKey = topic.shift();
+    switch(tpKey) {
+        case "checkin":
+            onMsgUpBsCheckin(topic, message);
+            break;
+        default:
+            onMsgUpBsTarget(tpKey, topic, message);
+    }
+}
+
+onMsgUpBsCheckin = function(topic, message) {
+    console.log("checkin:", message.toString()); 
+}
+
+onMsgUpBsTarget = function(target, topic, message) {
+    var Fiber = Npm.require('fibers');
+    Fiber(function() {
+        console.log("I am in a fiber");
+        //console.log(contactAll.collec.findOne({direction:"input"}));
+        if (Meteor.users.findOne({username: target}) !== undefined) {
+            console.log("message from:", target);
+            var tpKey = topic.shift();
+            switch(tpKey) {
+                case "input":
+                    onMsgUpBsTargetInput(target, topic, message);
+                break;
+                default:
+                    console.log("unsupported topic key:", tpKey );
+            }
+        } else {
+            console.log("unrecognized user:", target);
+        }
+    }).run();
+}
+
+onMsgUpBsTargetInput = function(target, topic, message) {
+    var tpKey = topic.shift();
+    contactAll.collec.update({owner:target, localId:tpKey}, {$set:{value:message.toString()}});
+    console.log("update input:", target, tpKey, message);
+}
