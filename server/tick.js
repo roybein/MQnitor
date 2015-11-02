271051c4c74813032fc66a76add1ccf1a3079225
roybein <roybein@gmail.com>
2015-11-02 23:22:44 +0800

    remove tick log

--- server/tick.js
+++ server/tick.js
@@ -2,7 +2,7 @@ setInterval(function() {
     var Fiber = Npm.require("fibers");
     Fiber(function() {
         var date = new Date();
-        console.log("tick: ", date);
+        //console.log("tick: ", date);
         var hour = date.getHours();
         if (hour > 12) {
            var time = (hour % 12).toString() + ":" + date.getMinutes() + " " + "PM";
