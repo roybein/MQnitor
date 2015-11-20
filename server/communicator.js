3aaac9561d98763d2d38f2a6a456e06acca7ebd4
roybein <roybein@gmail.com>
2015-11-20 23:20:11 +0800

    clean log of bundle update

--- server/communicator.js
+++ server/communicator.js
@@ -162,13 +162,11 @@ onMsgUpBsTargetInput = function(target, topic, message) {
     switch(tpKey) {
         case "bundle":
             try {
-                //var value = JSON.parse(JSON.stringify(message.toString()));
                 if (message[message.length -1] == '\0') {
                     message.slice(0, message.length - 1);
                 }
                 var value = JSON.parse(message.toString());
                 for (var k in value) {
-                    console.log(k, Number(value[k]));
                     contactAll.collec.update({owner:target, localName:k},
                         {$set:{value:Number(value[k])}});
                 }
